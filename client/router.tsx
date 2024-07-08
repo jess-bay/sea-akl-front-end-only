583aa6c772f7dc5dfc3ef6c5d624364dcc39c838
Jess Bayman <justjessb@gmail.com>
2024-07-08 15:00:54 +1200

    Change necessary layouts

--- client/router.tsx
+++ client/router.tsx
@@ -1,21 +1,18 @@
-/* eslint-disable react/jsx-key */
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
 } from 'react-router-dom'
-import App from './components/App'
-import Home from './components/Home'
-import React from 'react'
-import AboutFAQ from './components/AboutFAQ'
+import App from './components/App.tsx'
+import Home from './components/Home.tsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index element={<Home />} />
-      <Route path="/about" element={<AboutFAQ />} />
     </Route>
   )
 )
 
 export default router
+
