58113df4afbcbf77fbd091741afb2d2568fd8d67
Jess Bayman <justjessb@gmail.com>
2024-07-09 12:25:11 +1200

    Add basic styling to footer

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -1,26 +1,52 @@
 import React from 'react'
 
 function Footer() {
+  const footerStyle = {
+    color: '#FFF',
+    textDecoration: 'underline',
+  }
+
   return (
     <>
-      {' '}
       {/* API for weather at the bay */}
       {/* API for map to show location */}
-      <div>
-        <h3>Send your questions to us: hello@sea-auckland.nz</h3>
-      </div>
-      <div>
-        <h3>Hear About Special Tours First</h3>
-        <p>Subscribe</p>
-      </div>
-      <div>
-        <h3>
-          Call Us For Anything Else: <a>+64 20 4 SEAAKL</a>
-        </h3>
-      </div>
-      {/* Link to the pdf of t's and c's */}
-      <div>
-        <h3>Full T's&C's</h3>
+      <div
+        style={{
+          backgroundColor: '#007BFF',
+          color: '#FFF',
+          padding: '20px',
+          textAlign: 'center',
+        }}
+      >
+        <div style={{ marginBottom: '10px' }}>
+          <h3>
+            Send your questions to us:{' '}
+            <a href="mailto: hello@sea-auckland.nz" target="_blank">
+              hello@sea-auckland.nz
+            </a>
+          </h3>
+        </div>
+        <div style={{ marginBottom: '10px' }}>
+          <h3>
+            Hear About Special Tours First by <a href="">Subscribing Here</a>
+          </h3>
+        </div>
+        <div style={{ marginBottom: '10px' }}>
+          <h3>
+            Call Us For Anything Else:{' '}
+            <a href="tel:+64204732255">+64 20 4 SEAAKL</a>
+          </h3>
+        </div>
+        {/* Link to the pdf of t's and c's */}
+        <div>
+          <h3>
+            {' '}
+            Click Here To View The{' '}
+            <a href="client/public/tcs.pdf" target="_blank">
+              Full T's&C's
+            </a>
+          </h3>
+        </div>
       </div>
     </>
   )
