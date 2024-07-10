df079ed9675c89d9ac4b74e551bcd2038bf68235
Jess Bayman <justjessb@gmail.com>
2024-07-10 15:25:29 +1200

    Style reviews component and add locations of tours

--- client/components/Reviews.tsx
+++ client/components/Reviews.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { CSSProperties, useState } from 'react'
 
 function Reviews() {
   const [isHovered, setIsHovered] = useState<number | null>(null)
@@ -13,27 +13,27 @@ function Reviews() {
     {
       id: 1,
       imageUrl: 'client/public/customertestimonial1.png',
-      title: 'Tour 1 Testimonial',
+      title: 'City Tour',
     },
     {
       id: 2,
       imageUrl: 'client/public/customertestimonial2.png',
-      title: 'Tour 2 Testimonial',
+      title: 'Gulf Tour',
     },
     {
       id: 3,
       imageUrl: 'client/public/customertestimonial3.png',
-      title: 'Tour 3 Testimonial',
+      title: 'Harbour Tour',
     },
     {
       id: 4,
       imageUrl: 'client/public/customertestimonial4.png',
-      title: 'Tour 4 Testimonial',
+      title: 'Gulf Tour',
     },
     {
       id: 5,
       imageUrl: 'client/public/customertestimonial5.png',
-      title: 'Tour 5 Testimonial',
+      title: 'Sunset Tour',
     },
   ]
 
@@ -45,9 +45,36 @@ function Reviews() {
     setIsHovered(null)
   }
 
+  const buttonStyle: CSSProperties = {
+    backgroundColor: '#007BFF',
+    color: '#FFF',
+    border: 'none',
+    padding: '10px 20px',
+    margin: '50px',
+    borderRadius: '5px',
+    cursor: 'pointer',
+    fontSize: '2rem',
+  }
+
+  const headingStyle: CSSProperties = {
+    textAlign: 'center',
+    borderTop: '4px solid black',
+    paddingTop: '10px',
+    borderBottom: '4px solid black',
+    paddingBottom: '10px',
+    maxWidth: '40%',
+    margin: '0 auto',
+  }
+
   return (
     <>
-      <div style={{ textAlign: 'center', marginBottom: '20px' }}>
+      <div
+        style={{
+          ...headingStyle,
+          marginTop: '100px',
+          backgroundColor: 'rgba(255, 255, 255)',
+        }}
+      >
         <h1>Check Out Some Of Our Customer Testimonials!</h1>
       </div>
       <div style={{ textAlign: 'center', marginBottom: '20px' }}>
@@ -56,7 +83,7 @@ function Reviews() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <button>Leave Us A Review On Google Here!</button>
+          <button style={buttonStyle}>Leave Us A Review On Google Here!</button>
         </a>
       </div>
       <div
